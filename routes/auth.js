f1333480fc6c92b4f90665746832855d840e3239
shazoak <uunderthemoonlight@gmail.com>
2020-09-13 15:30:21 +0430

    user can log in and credentials are being checked if there is not  an error and a user exists it returns a web token

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const {check,validationResult} = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const User = require('../modals/Users');
+
 
 
 
@@ -16,8 +22,54 @@ router.get('/',(req,res)=>{
 //@ desc      Auth user & get token
 //@access     Public
 
-router.post('/',(req,res)=>{
-    res.send('Log in users');
+router.post('/',[
+    check('email','Please include a valid email').isEmail(),
+    check('password','Password id required').exists(),
+
+],async (req,res)=>{
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+
+    const {email,password} = req.body;
+    try {
+        let user = await User.findOne({email});
+
+        if (!user){
+            return res.status(400).json({msg:'Invalid Credentials'});
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password);
+
+        if(!isMatch){
+            return res.status(400).json({msg:'invalid credentials'})
+        }
+
+        const payload={
+            user:{
+                id:user.id
+            }
+        };
+
+        jwt.sign(payload,config.get('jwtSecret'),{
+            expiresIn: 360000
+        },(err,token)=>{
+            if (err){
+                throw err;
+            }
+            else {
+                res.json({token});
+            }
+        });
+
+
+
+    }catch (e) {
+        console.error(e.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const {check,validationResult} = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
 
 
 const User = require('../modals/Users');
@@ -36,7 +39,24 @@ router.post('/',[
 
         await user.save();
 
-        res.send('User saved');
+        // res.send('User saved');
+
+        const payload={
+            user:{
+                id:user.id
+            }
+        };
+
+        jwt.sign(payload,config.get('jwtSecret'),{
+            expiresIn: 360000
+        },(err,token)=>{
+            if (err){
+                throw err;
+            }
+            else {
+                res.json({token});
+            }
+        });
 
 
     }
